699ca54c2380c19dbd32017a388ae2ab049b6721
Ady Ngom <ady.touba.ngom@gmail.com>
2020-01-12 23:18:18 -0500

    finished porting over HTML and JS code to Angular

--- src/app/playlists/playlists.component.ts
+++ src/app/playlists/playlists.component.ts
@@ -19,6 +19,8 @@ export class PlaylistsComponent implements OnInit, AfterViewInit {
   title = "Upcoming must watch";
 
   listStyles = {};
+  episodes = [];
+  headers = [];
 
   @ViewChild("containerRef", { static: false }) containerRef: ElementRef;
   @ViewChild("videoBoxRef", { static: false }) videoBoxRef: ElementRef;
@@ -31,14 +33,35 @@ export class PlaylistsComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     this.setVideosListStyles();
 
-    const headers = [
+    this.headers = [
       ...this.containerRef.nativeElement.querySelectorAll(".chapter-header")
     ];
-    console.log(headers);
-    const episodes = [
+    this.episodes = [
       ...this.containerRef.nativeElement.querySelectorAll(".episodes")
     ];
-    console.log(episodes);
+
+    this.switchVideo("KrEWXL9ClGw");
+    this.setEpisodesHeight();
+
+    this.chaptersRef.nativeElement.addEventListener("click", e => {
+      e.preventDefault();
+      const elem = e.target;
+
+      if (elem.classList.contains("chapter-header")) {
+        const _episodes = elem.nextElementSibling;
+        this.episodes.forEach(ep => (ep.style.height = "0px"));
+        this.headers.forEach(header => header.classList.remove("open"));
+        elem.classList.toggle("open");
+        _episodes.style.height = _episodes.getAttribute("data-height") + "px";
+      }
+
+      if (elem.classList.contains("episode")) {
+        const { type, episodeid: id } = elem.dataset;
+        if (!!id) {
+          this.switchVideo(id, type);
+        }
+      }
+    });
   }
 
   private setVideosListStyles(): void {
@@ -46,13 +69,29 @@ export class PlaylistsComponent implements OnInit, AfterViewInit {
       height: `${this.videoPlayerRef.nativeElement.scrollHeight}px`,
       overflow: "scroll"
     };
-    console.log(this.videosListRef.nativeElement.style);
     Object.assign(this.videosListRef.nativeElement.style, this.listStyles);
   }
+
+  private setEpisodesHeight = (): void => {
+    !!this.episodes.length &&
+      this.episodes.forEach((ep, i) => {
+        const _height = ep.scrollHeight;
+        ep.setAttribute("data-height", _height);
+      });
+
+    this.episodes[0].style.height =
+      this.episodes[0].getAttribute("data-height") + "px";
+  };
 
   protected getYouTubeUrl = (id: string): string =>
     `https://www.youtube.com/embed/${id}?rel=0&amp;showinfo=0`;
 
   protected getVimeoUrl = (id: string): string =>
     `https://player.vimeo.com/video/${id}`;
+
+  protected switchVideo = (id, type = "youtube") => {
+    const url =
+      type === "vimeo" ? this.getVimeoUrl(id) : this.getYouTubeUrl(id);
+    this.videoPlayerRef.nativeElement.setAttribute("src", url);
+  };
 }
